1f5065dac4fb704b62bb83804c746fe9d6e0addd
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-01 13:59:35 +0330

    yes

--- galleryney/settings.py
+++ galleryney/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.AppConfig',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
