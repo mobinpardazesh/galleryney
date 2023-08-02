a0fa1dca09844669df63ab764b1db0ef6acbd8b0
Mobin Pardazesh <mobinpardazesh@gmail.com>
2023-08-02 22:32:19 +0330

    1402-05-11

--- common/models.py
+++ common/models.py
@@ -4,3 +4,11 @@ from django.db import models
 class Countries (models.Model):
     country_name=models.CharField(max_length=200)
     country_create_date=models.DateTimeField()
+
+class Proviences (models.Model):
+    provience_name=models.CharField(max_length=200)
+    provience_create_date=models.DateTimeField()
+
+class Cities (models.Model):
+    city_name=models.CharField(max_length=200)
+    city_create_date=models.DateTimeField()
