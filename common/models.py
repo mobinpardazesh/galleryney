937bcb01bce19f21a6232ec48affa7880969601a
Mobin Pardazesh <mobinpardazesh@gmail.com>
2023-08-02 22:27:03 +0330

    1402-05-11

--- common/models.py
+++ common/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Countries (models.Model):
+    country_name=models.CharField(max_length=200)
+    country_create_date=models.DateTimeField()
