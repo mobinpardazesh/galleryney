36495edd8f3a743c0c2d4e19b4fe4a2bbca3ae84
Mobin Pardazesh <mobinpardazesh@gmail.com>
2023-08-02 21:49:09 +0330

    1402-05-11

---
+++ mediahelpers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MediahelpersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mediahelpers'
---
+++ scheduletasks/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScheduletasksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scheduletasks'
