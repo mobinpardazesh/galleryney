698488f97d2c4c680459c9a74e67f072a416daf0
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-03 13:57:43 +0330

    1402/05/12

--- common/admin.py
+++ common/admin.py
@@ -4,14 +4,14 @@ from .models import Countries, Proviences, Cities
 
 @admin.register(Countries)
 class CountriesAdmin(admin.ModelAdmin):
-    list_display = ("country_name")
+    list_display = ("country_name",)
 
 
 @admin.register(Proviences)
 class ProviencesAmin(admin.ModelAdmin):
-    list_display = ("provience_name")
+    list_display = ("provience_name",)
 
 
 @admin.register(Cities)
 class CitiesAdmin(admin.ModelAdmin):
-    list_display = ("citoes_name")
+    list_display = ("citoes_name",)
