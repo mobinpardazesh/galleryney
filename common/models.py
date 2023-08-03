84283455fd6190901cde4f252a0fe408a96317f0
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-03 14:05:23 +0330

    1402/05/12

--- common/models.py
+++ common/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Countries(models.Model):
-    country_name = models.CharField(max_length=200)
+    country_name = models.CharField(max_length=200,default="Country Name")
 
     def __str__(self):
         return self.country_name
