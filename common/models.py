8354a1175029bc7decaeba0070b4807485685cf6
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-03 15:36:42 +0330

    1402/05/12

--- common/models.py
+++ common/models.py
@@ -2,18 +2,18 @@ from django.db import models
 # Create your models here.
 
 class Country(models.Model):
-    country_name = models.CharField(max_length=200,default="iran",unique=True)
+    country_name = models.CharField(max_length=200,default="iran")
     def __str__(self):
         return self.country_name
 
 class Provience(models.Model):
     provience_name = models.CharField(max_length=200)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE,unique=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
     def __str__(self):
         return self.provience_name
 
 class City(models.Model):
-    city_name = models.CharField(max_length=200,unique=True)
+    city_name = models.CharField(max_length=200)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     provience = models.ForeignKey(Provience, on_delete=models.CASCADE)
 
