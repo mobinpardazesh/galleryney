e3843d1426ae36527191b98e461a632db799e930
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-03 13:15:18 +0330

    1402/05/12

--- common/admin.py
+++ common/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from .models import Countries
+from .models import Countries, Proviences, Cities
+
 
 class CountriesAdmin(admin.ModelAdmin):
     list_display = ("country_name")
 
+
 admin.site.register(Countries)
+
+
+class ProviencesAmin(admin.ModelAdmin):
+    list_display = ("provience_name")
+
+
+admin.site.register(Proviences)
+
+
+class CitiesAdmin(admin.ModelAdmin):
+    list_display = ("citoes_name")
+
+
+admin.site.register(Cities)
--- common/models.py
+++ common/models.py
@@ -8,3 +8,17 @@ class Countries(models.Model):
 
     def __str__(self):
         return self.country_name
+
+
+class Proviences(models.Model):
+    provience_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.provience_name
+
+
+class Cities(models.Model):
+    city_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.city_name
