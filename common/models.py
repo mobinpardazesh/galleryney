39484fd5b79711d751dd76b73a7ebd4433b268e4
Mobin Pardazesh <mobinpardazesh@gmail.com>
2023-08-03 20:40:25 +0330

    1402-05-11

--- common/models.py
+++ common/models.py
@@ -4,10 +4,9 @@ from django.utils import timezone
 
 class Country(models.Model):
     is_country_active=models.BooleanField(default=True)
-    country_name = models.CharField('Country',max_length=200,default="iran" ,null=True, blank=True)
-    country_code = models.CharField('Code', max_length=10, null=True)
+    country_name = models.CharField('Country',max_length="200",default="iran" ,null=True, blank=True)
+    country_code = models.CharField('Code', max_length="10", null=True)
     country_center=models.CharField('Country Center',max_length="200")
-    is_country_active=models.BooleanField(default=True)
     country_iso_code_short = models.CharField('ISO Code Short', max_length=2, null=True)
     country_iso_code_long = models.CharField('ISO Code Long', max_length=3, null=True)
     country_createdate=models.DateTimeField(default=timezone.now)
@@ -17,8 +16,8 @@ class Country(models.Model):
 
 class Provience(models.Model):
     is_provience_active=models.BooleanField(default=True)
-    provience_name = models.CharField('Provience',max_length=200,null=True)
-    provience_center = models.CharField('Code', max_length=10, null=True)
+    provience_name = models.CharField('Provience',max_length="200",null=True)
+    provience_center = models.CharField('Code', max_length="10", null=True)
     provience_center_code = models.CharField('Provience Code', max_length=10, null=True)
     is_country_center=models.BooleanField(default=False)
     provience_createdate=models.DateTimeField(default=timezone.now)
@@ -28,7 +27,7 @@ class Provience(models.Model):
 
 class City(models.Model):
     is_city_active=models.BooleanField(default=True)
-    city_name = models.CharField('City',max_length=200,null=True)
+    city_name = models.CharField('City',max_length="200",null=True)
     is_provience_center=models.BooleanField(default=False)
     city_createdate=models.DateTimeField(default=timezone.now)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
