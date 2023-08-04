193fe566a8da497cebf532fe32b7da3ff52861e7
Mobin Pardazesh <mobinpardazesh@gmail.com>
2023-08-04 21:05:04 +0330

    1402-05-11

--- stores/models.py
+++ stores/models.py
@@ -14,13 +14,13 @@ class Storemodel (models.Model):
     sslenabled=models.BooleanField(default=False)
     hostvalues=models.CharField(max_length=200)
     defaultlanguage=models.ForeignKey(Language,on_delete=models.CASCADE)
-    displayorder=models.IntegerField(max_length=5)
+    displayorder=models.IntegerField()
     storecountryname=models.ForeignKey(Countrymodel,on_delete=models.CASCADE)
     storeproviencename=models.ForeignKey(Proviencemodel,on_delete=models.CASCADE)
     storecityname=models.ForeignKey(Citymodel,on_delete=models.CASCADE)
     storeaddress=models.CharField(max_length=200)
     storephonenumber=models.CharField(max_length=100)
-    storevat=models.IntegerField(max_length=2)
+    storevat=models.IntegerField()
 
     def __str__(self):
         return self.storename
